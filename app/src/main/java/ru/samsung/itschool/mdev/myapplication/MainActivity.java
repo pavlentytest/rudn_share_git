ddf0712213fb99402e1e9fd455d8ae0c2520d0f5
Pavel <impetus@yandex.ru>
2022-11-15 20:22:15 +0300

    Добавил комментарий

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // комментарий
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
